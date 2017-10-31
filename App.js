805e8a027ef6f2c463ac6d64490e43cef041a5e3
André Schilipack <andre.schilipack@gmail.com>
2017-10-31 21:30:18 -0200

    header layout first version

--- App.js
+++ App.js
@@ -23,20 +23,38 @@
 // });
 
 import React from 'react';
+import { View, Text, Platform, StatusBar } from 'react-native';
 import { MapView } from 'expo';
+import { Ionicons, Octicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+
+
+      <View style={{ flex: 1, top: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight }}>
+        <View
+          style={{
+            maxHeight: 40,
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <Text>Find a location...</Text>
+          <Octicons style={{}} name="three-bars" size={32} color="gray" />
+        </View>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     );
   }
 }
